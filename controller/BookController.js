a6732d8ab628e585e5e6f8b2612b823616966d9b
jeung <ung8309@gmail.com>
2025-10-23 19:53:14 +0900

    likes

--- controller/BookController.js
+++ controller/BookController.js
@@ -6,7 +6,7 @@ const allBooks = (req,res) => {
 
     let offset = limit * (currentPage-1);
     
-    let sql = "SELECT * FROM books";
+    let sql = "SELECT *, (SELECT count( *) FROM likes WHERE books.id=liked_book_id) AS likes FROM books";
     let values = [];
     if(category_id && news) {
         sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
@@ -36,10 +36,17 @@ const allBooks = (req,res) => {
 };
 
 const bookDetail = (req,res) => {
-    let {id} = req.params;
-    let sql = `SELECT * FROM books LEFT JOIN category
-                ON books.category_id = category.id WHERE books.id = ?`;
-    conn.query(sql, id,
+    let {user_id} =req.body;
+    let book_id = req.params.id;
+    let sql = `SELECT *,
+                    (SELECT count(*) FROM likes WHERE books.id=liked_book_id) AS likes,
+                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+                FROM books 
+                LEFT JOIN category
+                ON books.category_id = category.category.id 
+                WHERE books.id=?`;
+        let values = [user_id, book_id, book_id];
+        conn.query(sql, values,
         (err, results) => {
             if(err) {
                 console.log(err);
